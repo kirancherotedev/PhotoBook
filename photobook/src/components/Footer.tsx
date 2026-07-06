ce039bd05033695d3651ede5a585b1a6e130f0be
kirancherotedev <mailboxkirancherote@gmail.com>
2026-07-06 16:33:24 +0530

    fix: restore Legal comment in Footer.tsx

--- photobook/src/components/Footer.tsx
+++ photobook/src/components/Footer.tsx
@@ -129,7 +129,7 @@ export default function Footer() {
             </div>
           </div>
 
-          {/* Le */}
+          {/* Legal */}
           <div>
             <h4
               style={{
